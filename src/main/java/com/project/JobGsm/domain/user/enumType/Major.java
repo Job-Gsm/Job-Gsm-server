a16b188fc39bc107248a2a66e36e0d4b46cb63d1
SungGil-5125 <sunggil0125@naver.com>
2022-08-24 12:25:56 +0900

    ✨ :: Add major

---
+++ src/main/java/com/project/JobGsm/domain/user/enumType/Major.java
@@ -0,0 +1,9 @@
+package com.project.JobGsm.domain.user.enumType;
+
+import lombok.Getter;
+
+@Getter
+public enum Major {
+    BACK_END, FRONT_END, DESIGNER, PROJECT_MANAGER, ANDROID, IOS, DATABASE_ADMINISTATOR
+
+}
