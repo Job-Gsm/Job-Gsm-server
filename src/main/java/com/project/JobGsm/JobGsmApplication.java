8a460f53f664ac8efe8e51df5b6da0b530fa982b
SungGil-5125 <sunggil0125@naver.com>
2022-08-24 12:28:03 +0900

    ✨ :: naming

---
+++ src/main/java/com/project/JobGsm/JobGsmApplication.java
@@ -0,0 +1,13 @@
+package com.project.JobGsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobGsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobGsmApplication.class, args);
+	}
+
+}
