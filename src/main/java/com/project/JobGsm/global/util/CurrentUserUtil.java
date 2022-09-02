d0336742f67a20dbe5e897a2fdae31cc6240ce1a
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-09-02 13:55:25 +0900

    :sparkles: :: CurrentUserUtil 추가

---
+++ src/main/java/com/project/JobGsm/global/util/CurrentUserUtil.java
@@ -0,0 +1,29 @@
+package com.project.JobGsm.global.util;
+
+import com.project.JobGsm.domain.sign.repository.UserRepository;
+import com.project.JobGsm.domain.user.User;
+import com.project.JobGsm.global.exception.exceptions.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import static com.project.JobGsm.global.exception.ErrorCode.USER_NOT_FOUND;
+
+@Component
+@RequiredArgsConstructor
+public class CurrentUserUtil {
+
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        String email;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(principal instanceof UserDetails) {
+            email = ((User) principal).getEmail();
+        } else {
+            email = principal.toString();
+        }
+        return userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND));
+    }
+}
