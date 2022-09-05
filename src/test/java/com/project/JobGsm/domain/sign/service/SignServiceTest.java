ad6cd7bffc5354e1bdf6bd4017c54e4e601b7b73
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-09-05 11:28:53 +0900

    :sparkles: :: email 발송, 인증 테스트

--- src/test/java/com/project/JobGsm/domain/sign/service/SignServiceTest.java
+++ src/test/java/com/project/JobGsm/domain/sign/service/SignServiceTest.java
@@ -1,9 +1,12 @@
 package com.project.JobGsm.domain.sign.service;
 
+import com.project.JobGsm.domain.sign.dto.request.CheckEmailKeyDto;
+import com.project.JobGsm.domain.sign.dto.request.EmailDto;
 import com.project.JobGsm.domain.sign.dto.request.SignInDto;
 import com.project.JobGsm.domain.sign.dto.request.SignUpDto;
 import com.project.JobGsm.domain.sign.dto.response.UserSignInResponseDto;
 import com.project.JobGsm.domain.user.repository.UserRepository;
+import com.project.JobGsm.global.util.RedisUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,7 @@ class SignServiceTest {
 
     @Autowired private SignService signService;
     @Autowired private UserRepository userRepository;
+    @Autowired private RedisUtil redisUtil;
 
     @Test
     @DisplayName("회원가입 테스트")
@@ -53,9 +57,34 @@ class SignServiceTest {
     }
 
     @Test
-    @DisplayName("회원가입 이메일 테스트")
+    @DisplayName("회원가입 이메일 발송 테스트")
     void signupEmail() {
 
+        // given
+        EmailDto emailDto = EmailDto.builder()
+                .email("s21023@gsm.hs.kr")
+                .build();
+
+        // when
+        String authKey = signService.signupSendEmail(emailDto);
+        System.out.println("authKey = " + authKey);
+
+        // then
+        assertThat(authKey).isNotNull();
+
+    }
+
+    @Test
+    @DisplayName("인증키 확인 테스트")
+    void checkEmail() {
+
+        // given
+        CheckEmailKeyDto checkEmailKeyDto = CheckEmailKeyDto.builder()
+                .key("29931")
+                .build();
+
+        // when, then
+        signService.checkEmailKey(checkEmailKeyDto);
 
     }
 
