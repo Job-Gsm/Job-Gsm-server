aed50fe379a8666845722dd16e99b8c953ef7d28
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-09-06 15:04:28 +0900

    :sparkles: :: baseTimeEntity 추가

---
+++ src/main/java/com/project/JobGsm/global/entity/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package com.project.JobGsm.global.entity;
+
+import lombok.Getter;
+import net.bytebuddy.asm.Advice;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createDate;
+
+    @LastModifiedBy
+    private LocalDateTime modifiedDate;
+}
