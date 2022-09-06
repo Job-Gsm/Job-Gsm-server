071f5da71e21f53c1d6cdaa171de691bac67f348
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-09-06 21:09:25 +0900

    :sparkles: :: modelmapper setter 추가

--- src/main/java/com/project/JobGsm/domain/board/dto/request/GetBoardDto.java
+++ src/main/java/com/project/JobGsm/domain/board/dto/request/GetBoardDto.java
@@ -1,10 +1,7 @@
 package com.project.JobGsm.domain.board.dto.request;
 
 import com.project.JobGsm.domain.board.enumType.Major;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
@@ -12,11 +9,14 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Setter
 public class GetBoardDto {
 
     private String title;
     private String content;
     private List<Major> majors;
     private String deadline;
+    private String url;
+    private String view;
 
 }
