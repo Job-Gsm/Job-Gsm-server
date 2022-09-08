5172379ce667f00926a7a25526236a3559a61603
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-09-08 16:19:11 +0900

    :sparkles: :: board 업로드 test

---
+++ src/test/java/com/project/JobGsm/domain/board/service/BoardServiceTest.java
@@ -0,0 +1,69 @@
+package com.project.JobGsm.domain.board.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.JobGsm.domain.board.dto.request.BoardDto;
+import com.project.JobGsm.domain.board.enumType.Major;
+import com.project.JobGsm.domain.sign.dto.request.SignUpDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.util.List;
+
+@SpringBootTest
+class BoardServiceTest {
+
+    @Autowired
+    private BoardService boardService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void currentUser() {
+
+        SignUpDto signUpDto = SignUpDto.builder()
+                .email("s21010@gsm.hs.kr")
+                .password("123456")
+                .username("김성길")
+
+    }
+
+    @Test
+    @DisplayName("구직광고 업로드 테스트")
+    void writeBoard() {
+
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "imgae.jpeg", "image/jpeg", "<<jpeg>> data>>".getBytes());
+
+        boardService.writeBoard(
+                BoardDto.builder()
+                        .title("test")
+                        .content("test")
+                        .deadline("20220910")
+                        .majors(List.of(Major.BACK_END))
+                        .build(), mockMultipartFile
+                );
+
+
+    }
+
+    @Test
+    void updateBoard() {
+    }
+
+    @Test
+    void deleteBoard() {
+    }
+
+    @Test
+    void getAllBoard() {
+    }
+
+    @Test
+    void getBoardById() {
+    }
+}
