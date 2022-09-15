b451eceb068983a8abb09d227e4d70d3af6a435a
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-09-15 17:05:57 +0900

    :sparkles: :: 추후 클래스 이름을 변경해야할듯..

--- src/main/java/com/project/JobGsm/domain/user/dto/request/SelectMajorDto.java
+++ src/main/java/com/project/JobGsm/domain/user/dto/request/SelectMajorDto.java
@@ -3,18 +3,35 @@ package com.project.JobGsm.domain.user.dto.request;
 
 import com.project.JobGsm.domain.user.enumType.Major;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class SelectMajorDto {
+
+    @Pattern(regexp = "^[a-zA-Z0-9]+@gsm.hs.kr$", message = "학교계정을 입력해주세요")
+    @NotBlank
+    private String email;
 
     @NotBlank
     private Major major;
 
+    @NotBlank
+    private String github;
+
+    @NotBlank
+    private String discord;
+
+    @Size(min = 1, max = 7)
+    @NotBlank
+    private int career;
 
 }
