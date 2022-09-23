91343c4f389dea5e7fd754b4d8210bef71f2bf26
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-09-23 12:19:23 +0900

    :sparkles: :: BaseTimeEntity용 EnableJpaAuditing 추가

--- src/main/java/com/project/JobGsm/JobGsmApplication.java
+++ src/main/java/com/project/JobGsm/JobGsmApplication.java
@@ -2,8 +2,11 @@ package com.project.JobGsm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JobGsmApplication {
 
 	public static void main(String[] args) {
